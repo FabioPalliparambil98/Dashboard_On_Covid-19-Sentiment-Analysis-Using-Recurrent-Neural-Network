b62f55fcfdd795b54df812eeff3e471d2ab1cdc5
Fabio Palliparambil <fabiolearning1998@gmail.com>
2021-04-29 21:54:30 +0100

    app

--- application.py
+++ application.py
@@ -10,24 +10,7 @@ import hashtag
 
 application = Flask(__name__)
 
-df_general_filter = pd.read_csv('https://raw.githubusercontent.com/FabioPalliparambil98/cleaned-covid-dataset/main/general_covid.csv')
-df_restriction_filter = pd.read_csv('https://raw.githubusercontent.com/FabioPalliparambil98/cleaned-covid-dataset/main/covid_restriction.csv')
-df_vaccination_filter = pd.read_csv('https://raw.githubusercontent.com/FabioPalliparambil98/cleaned-covid-dataset/main/covid_vaccination.csv')
 
-
-df_general_filter_sentiment = df_general_filter.copy()
-df_restriction_filter_sentiment = df_restriction_filter.copy()
-df_vaccination_filter_sentiment = df_vaccination_filter.copy()
-
-
-df_general_filter['sentiment'].replace(0, 'negative', inplace=True)
-df_general_filter['sentiment'].replace(1, 'positive', inplace=True)
-
-df_restriction_filter['sentiment'].replace(0, 'negative', inplace=True)
-df_restriction_filter['sentiment'].replace(1, 'positive', inplace=True)
-
-df_vaccination_filter['sentiment'].replace(0, 'negative', inplace=True)
-df_vaccination_filter['sentiment'].replace(1, 'positive', inplace=True)
 """
 It displays the Home Pages of the Visualisations.
 """
@@ -35,9 +18,9 @@ It displays the Home Pages of the Visualisations.
 
 @application.route('/')
 def homepage():
-    sentiment_general = sentiment_data(df_general_filter_sentiment)
-    sentiment_vaccination = sentiment_data(df_restriction_filter_sentiment)
-    sentiment_restriction = sentiment_data(df_vaccination_filter_sentiment)
+    sentiment_general = 'negative'
+    sentiment_vaccination = 'negative'
+    sentiment_restriction = 'negative'
 
     plot_general, plot_restriction, plot_vaccination, line_general, line_restriction,line_vaccination, all_line_scatters,scatter_circles,pie_general = hashtag.create_plot(hashtag.df_general_hash_tag,
                                                                            hashtag.df_restriction_hash_tag,
@@ -57,16 +40,6 @@ def homepage():
                            sentiment_vaccination=sentiment_vaccination,
                            sentiment_restriction=sentiment_restriction,
                            plots=plots)
-
-
-def sentiment_data(df):
-    df_sentiment = df['sentiment'].value_counts()
-    negative = df_sentiment.loc[0]
-    positive = df_sentiment.loc[1]
-    if negative > positive:
-        return "negative"
-    else:
-        return "positive"
 
 
 if __name__ == '__main__':
